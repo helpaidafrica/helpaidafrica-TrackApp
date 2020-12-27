d850d39b1fdb4d4ca19ebe106ea8cd729d769c23
Syed A Bilgrami <bilgrami@gmail.com>
2020-12-27 02:21:09 -0800

    implemented photo upload by box

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import aws_exports from "./aws-exports";
 
-import { makeStyles } from "@material-ui/core/styles";
-
 import Button from "@material-ui/core/Button";
-import Image from "material-ui-image";
 import { DataGrid } from "@material-ui/data-grid";
-
-// import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
 
 import logo from "./logo.jpeg";
 import "./App.css";
@@ -16,19 +11,22 @@ import "./App.css";
 import { Amplify, API, Storage } from "aws-amplify";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { listBoxs } from "./graphql/queries";
-import { createImage as createImageMutation } from "./graphql/mutations";
+import {
+  createImage as createImageMutation,
+  updateBox as updateBoxMutation,
+} from "./graphql/mutations";
 
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from "uuid";
 
 Amplify.configure(aws_exports);
 
-const initialFormState = { id: "0", label: "", src: "", altText: "", userID: "0" };
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+const initialFormState = {
+  id: "0",
+  label: "",
+  src: "",
+  altText: "",
+  userID: "1",
+};
 
 function App() {
   const [boxes, setBoxes] = useState([]);
@@ -38,12 +36,18 @@ function App() {
     fetchBoxes();
   }, []);
 
+  async function upload_to_s3(s3_path, file) {
+    const { key } = await Storage.put(s3_path, file);
+    return key;
+  }
+  function get_file_name(file_name) {
+    return new Date().toISOString() + " - " + uuid() + "-" + file_name;
+  }
+
   async function onChange(e) {
     if (!e.target.files[0]) return;
     const file = e.target.files[0];
-    const { key }  = await Storage.put(file.name, file);
-    setFormData({ ...formData, image: file.name, s3_path: 's3://haaapistorage01542-dev/public/' + key });
-    console.log('S3 Object key', key)
+    setFormData({ ...formData, image: file.name, file: file });
     fetchBoxes();
   }
 
@@ -51,48 +55,76 @@ function App() {
     const apiData = await API.graphql({
       query: listBoxs,
       variables: {
-        limit: 5,
+        limit: 1000,
       },
     });
-    console.log({ apiData })
+    // console.log({ apiData })
 
     const boxes = apiData.data.listBoxs.items;
     await Promise.all(
       boxes.map(async (box) => {
-        if (box.imageID) {
-          const image = await Storage.get(box.image.src);
-          box.main_image = image;
-        }
+        const img =
+          box.imageID && box.image && box.image.src
+            ? box.image.src
+            : "noimage.png";
+        const image = await Storage.get(img);
+        box.main_image = image;
         return box;
       })
     );
     setBoxes(apiData.data.listBoxs.items);
   }
 
-  async function addPhoto(box_qr_code) {
-    console.log("Future: Uploading photos for Box: " + box_qr_code);
-    if (!formData.id || !formData.label) return;
-    formData.label = formData.label || formData.image
-    formData.src = formData.s3_path
-    formData.id = formData.label + " - " + uuid()
+  async function addPhoto(box_id) {
+    if (!formData.image || !formData.label) {
+      alert("adding photos: Missing required params");
+      return;
+    }
 
+    // upload to s3
+    console.log("Uploading photos for Box: " + box_id);
+    const s3_path = get_file_name(formData.image);
+    await upload_to_s3(s3_path, formData.file);
+
+    // add image
+    const new_image_id = box_id + "-" + formData.label + "-" + uuid();
     const image_input = {
-      "id": formData.id,
-      "src": formData.src,
-      "label": formData.label,
-    }      
-    console.log('image_input: ', image_input)
-    const newImage =  await API.graphql({
+      id: new_image_id,
+      src: s3_path,
+      label: formData.label,
+      altText: formData.label,
+      userID: "1", // TODO: replace with logged-in user
+      isActive: true,
+      dateAdded: new Date().toISOString(),
+    };
+
+    const newImage = await API.graphql({
       query: createImageMutation,
-      variables: { input: image_input }
+      variables: { input: image_input },
+    });
+
+    console.log("new image added:", newImage);
+    if (newImage) {
+      const box_input = {
+        id: box_id,
+        imageID: new_image_id,
+      };
+
+      const updateBox = await API.graphql({
+        query: updateBoxMutation,
+        variables: { input: box_input },
       });
-    // console.log("new image:", newImage)
-    // if (formData.image) {
-    //   const image = await Storage.get(formData.image);
-    //   formData.image = image;
-    // }
-    // setBoxes([...boxes, formData]);
-    // setFormData(initialFormState);
+
+      console.log("Box updated:", updateBox);
+
+      console.log("Fetching image from s3:", updateBox);
+      const image = await Storage.get(s3_path);
+      formData.image = image;
+
+      console.log("Fetching image from s3:", updateBox);
+    }
+    fetchBoxes();
+    setFormData(initialFormState);
   }
 
   const YearOfDate = {
@@ -107,7 +139,7 @@ function App() {
       field: "org",
       headerName: "Org Name",
       width: 150,
-      renderCell: (params) => <span>{params.value.name}</span>,
+      renderCell: (params) => <span>{params.value ? params.value.name : "Unknown Org"}</span>,
     },
     { field: "status", headerName: "Status", width: 130 },
     {
@@ -122,19 +154,28 @@ function App() {
       type: "datetime",
       width: 250,
     },
+    // {
+    //   field: "imageID",
+    //   headerName: "Photo ID",
+    //   width: 400,
+    //   renderCell: (params) => (params.value.src!== undefined ? params.value.src  : ""),
+    // },
     {
       field: "main_image",
       headerName: "Box Photo",
-      width: 200,
+      width: 150,
+      // TODO: Display thumbnail
       renderCell: (params) => (
         <div>
-          <Image src={params.value} disableError={true} />
+          <a href={params.value} target="_blank" rel="noopener noreferrer">
+            <img alt={params.value} src={params.value} height="30" width="30" />
+          </a>
         </div>
       ),
     },
     {
       field: "id",
-      headerName: "Upload Photo",
+      headerName: " ",
       width: 200,
       renderCell: (params) => (
         <div>
@@ -163,10 +204,18 @@ function App() {
           alt="logo"
           style={{ height: 200, width: 200 }}
         />
-        HAA Box Tracker {" "}
-        {"- " + process.env.REACT_APP_BUILD_ENV}
+        HAA Box Tracker {"- " + process.env.REACT_APP_BUILD_ENV}
       </h1>
-
+      <hr />
+      <div style={{ height: 450, width: "90%" }}>
+        <DataGrid
+          rows={boxes}
+          columns={columns}
+          pageSize={6}
+          checkboxSelection={false}
+        />
+      </div>
+      <hr />
       <div
         style={{
           justifyContent: "center",
@@ -174,36 +223,18 @@ function App() {
         }}
       >
         <br />
-        Image Label: &nbsp;
+        Box Photo Label: &nbsp;
         <input
           onChange={(e) => setFormData({ ...formData, label: e.target.value })}
-          placeholder="Image Label"
+          placeholder="Please enter Box Photo Label"
           value={formData.label}
           maxLength="100"
           size="35"
         />
-        {/* <br />
-        Source URL: &nbsp;
-        <input
-          onChange={(e) => setFormData({ ...formData, url: e.target.value })}
-          placeholder="Source URL"
-          value={formData.url}
-          size="35"
-        /> */}
-        <br />
         &nbsp;
         <input type="file" onChange={onChange} />
       </div>
-      <hr />
-
-      <div style={{ height: 400, width: "100%" }}>
-        <DataGrid
-          rows={boxes}
-          columns={columns}
-          pageSize={3}
-          checkboxSelection={false}
-        />
-      </div>
+      <br />
 
       <AmplifySignOut />
     </div>
